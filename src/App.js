b88e303dce82c0ce42d0a47be4f54c5b1f7c71fc
an.skrykalov <an.skrykalov@tensor.ru>
2025-04-03 10:42:07 +0300

    `first commit`

--- src/App.js
+++
@@ -1,8 +0,0 @@
-define(["require", "exports", "tslib", "react/jsx-runtime"], function (require, exports, tslib_1, jsx_runtime_1) {
-    "use strict";
-    Object.defineProperty(exports, "__esModule", { value: true });
-    function App() {
-        return ((0, jsx_runtime_1.jsx)("div", tslib_1.__assign({ className: "App" }, { children: "\u0442\u0435\u0441\u0442" })));
-    }
-    exports.default = App;
-});
--- src/App.tsx
+++
@@ -1,9 +0,0 @@
-function App() {
-  return (
-    <div className="App">
-      тест
-    </div>
-  );
-}
-
-export default App;
